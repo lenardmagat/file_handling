4379be6d983334ba10c064839a33b3b27bec0706
lenardmagat <lenardmagat76@gmail.com>
2026-04-23 17:48:06 +0800

    making eve_odd_separator an OOP program

--- even_odd_separator/main.py
+++ even_odd_separator/main.py
@@ -1,31 +1,38 @@
 import logging
 logging.basicConfig(level=logging.INFO)
-PATH = "C:\\Users\\Marry Bless Magat\\Documents\\Lenard Files\\arduino ide\\file_handling\\even_odd_separator\\"
-numbers = []
-even = []
-odd = []
+class Even_odd_separator:
+    def __init__(self):
+        self.FILE_PATH = "C:\\Users\\Marry Bless Magat\\Documents\\Lenard Files\\arduino ide\\file_handling\\even_odd_separator\\"
+        self.even = []
+        self.odd = []
+    def create_file(self, file_name, data):
+        try:
+            with open(self.FILE_PATH + file_name, "w") as file:
+                file.write(" ".join(str(i) for i in data))
+            logging.info("Successfully created a file")
+        except Exception as e:
+            logging.warning(e)
+    def get_data(self):
+        numbers = []
+        with open(self.FILE_PATH + "numbers.txt", "r") as file:
+            try:
+                for line in file:
+                    numbers.append(float(line.strip()))
+                logging.info("Success reading the file")
+                return numbers
+            except Exception as e:
+                logging.warning(e)
 
-def create_file(file_name, data):
-    try:
-        with open(PATH + file_name, "w") as file:
-            file.write(" ".join(str(i) for i in data))
-        logging.info("Successfully created a file")
-    except Exception as e:
-        logging.warning(e)
+    def serate_even_and_odd_integer_and_save(self):
+        numbers = self.get_data()
+        for i in numbers:
+            if i % 2 == 0:
+                self.even.append(i)
+                continue
+            self.odd.append(i)
 
-with open(PATH + "numbers.txt", "r") as file:
-    try:
-        for line in file:
-            numbers.append(float(line.strip()))
-        logging.info("Success reading the file")
-    except Exception as e:
-        logging.warning(e)
+        self.create_file("even_numbers.txt", self.even)
+        self.create_file("odd_numbers.txt", self.odd)
 
-for i in numbers:
-    if i % 2 == 0:
-        even.append(i)
-        continue
-    odd.append(i)
-
-create_file("even_numbers.txt", even)
-create_file("odd_numbers.txt", odd)
+if __name__ == "__main__":
+    Even_odd_separator().serate_even_and_odd_integer_and_save()
