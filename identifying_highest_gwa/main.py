088dc29c73aca47e08c92d1890f3da1956a19791
lenardmagat <lenardmagat76@gmail.com>
2026-04-23 17:37:28 +0800

    making identifying_highest_gwa an OOP program

--- identifying_highest_gwa/main.py
+++ identifying_highest_gwa/main.py
@@ -1,19 +1,30 @@
 import json
 import logging
 logging.basicConfig(level=logging.INFO)
-PATH = "C:\\Users\\Marry Bless Magat\\Documents\\Lenard Files\\arduino ide\\file_handling\\identifying_highest_gwa\\class.json"
-gwa = []
-try:
-    with open(PATH, 'r') as file:
-        data = json.load(file)
-    logging.info("Successfully read the file")
-except Exception as e:
-    logging.warning(e)
 
-data_value = data.get("students")
-for i in data_value:
-    gwa.append(i.get("gwa"))
-logging.info("successfully obtain student's GWA")
-highest_grade = gwa.index(min(gwa))
-logging.info("successfully obtain student data who has highest GWA")
-print(f"{data_value[highest_grade].get('name')} got {data_value[highest_grade].get('gwa')} which is the highest in the class")
+
+class Student_class:
+    def __init__(self):
+        self.FILEPATH = "C:\\Users\\Marry Bless Magat\\Documents\\Lenard Files\\arduino ide\\file_handling\\identifying_highest_gwa\\class.json"
+
+    def get_students_data(self):
+        try:
+            with open(self.FILEPATH, 'r') as file:
+                data = json.load(file)
+            logging.info("Successfully read the file")
+            return data.get("students")
+        except Exception as e:
+            logging.warning(e)
+
+    def get_highest_gwa(self):
+        gwa = []
+        data = self.get_students_data()
+        for i in data:
+            gwa.append(i.get("gwa"))
+        logging.info("successfully obtain student's GWA")
+        highest_grade = gwa.index(min(gwa))
+        logging.info("successfully obtain student data who has highest GWA")
+        print(f"{data[highest_grade].get('name')} got {data[highest_grade].get('gwa')} which is the highest in the class")
+
+if __name__ == "__main__":
+    Student_class().get_highest_gwa()
