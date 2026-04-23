8c4a4329423edbad1ff51803e71e58928df529be
lenardmagat <lenardmagat76@gmail.com>
2026-04-23 16:12:03 +0800

    creating a function to open and write content on specific file, and adding exception handling also the logging system

--- even_odd_separator/main.py
+++ even_odd_separator/main.py
@@ -1,13 +1,31 @@
+import logging
+logging.basicConfig(level=logging.INFO)
 PATH = "C:\\Users\\Marry Bless Magat\\Documents\\Lenard Files\\arduino ide\\file_handling\\even_odd_separator\\"
 numbers = []
 even = []
 odd = []
+
+def create_file(file_name, data):
+    try:
+        with open(PATH + file_name, "w") as file:
+            file.write(" ".join(str(i) for i in data))
+        logging.info("Successfully created a file")
+    except Exception as e:
+        logging.warning(e)
+
 with open(PATH + "numbers.txt", "r") as file:
-    for line in file:
-        numbers.append(float(line.strip()))
+    try:
+        for line in file:
+            numbers.append(float(line.strip()))
+        logging.info("Success reading the file")
+    except Exception as e:
+        logging.warning(e)
 
 for i in numbers:
     if i % 2 == 0:
         even.append(i)
         continue
     odd.append(i)
+
+create_file("even_numbers.txt", even)
+create_file("odd_numbers.txt", odd)
