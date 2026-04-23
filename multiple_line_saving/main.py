b6e1b9344437f6d6a57e2f5d38f6ef1dc89936a9
lenardmagat <lenardmagat76@gmail.com>
2026-04-23 16:49:13 +0800

    creating a function that save user input and algorithm to make user input multple input

--- multiple_line_saving/main.py
+++ multiple_line_saving/main.py
@@ -1,4 +1,18 @@
 import logging
 PATH = "C:\\Users\\Marry Bless Magat\\Documents\\Lenard Files\\arduino ide\\file_handling\\multiple_line_saving\\mylife.txt"
+more_line = True
+def saving_user_input(data : str):
+    with open(PATH, "a") as file:
+        file.write("\n" + data)
 
-
+while more_line:
+    data = input("Enter line")
+    saving_user_input(data)
+    while True:
+        check_status = input("Are there more lines y/n: ").strip().lower()
+        if check_status == "y":
+            break
+        elif check_status == "n":
+            more_line = False
+            break
+        continue
