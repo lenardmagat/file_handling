6e7db59265753efee4b60ec56d6c1a5072398c8c
lenardmagat <lenardmagat76@gmail.com>
2026-04-23 16:38:35 +0800

    creating a algorithm that read a class file and algorithm that finds the highest gwa in the class

--- identifying_highest_gwa/main.py
+++ identifying_highest_gwa/main.py
@@ -1,3 +1,19 @@
 import json
 import logging
-PATH = "C:\Users\\Marry Bless Magat\\Documents\\Lenard Files\\arduino ide\\file_handling\\identifying_highest_gwa\\class.json"
+logging.basicConfig(level=logging.INFO)
+PATH = "C:\\Users\\Marry Bless Magat\\Documents\\Lenard Files\\arduino ide\\file_handling\\identifying_highest_gwa\\class.json"
+gwa = []
+try:
+    with open(PATH, 'r') as file:
+        data = json.load(file)
+    logging.info("Successfully read the file")
+except Exception as e:
+    logging.warning(e)
+
+data_value = data.get("students")
+for i in data_value:
+    gwa.append(i.get("gwa"))
+logging.info("successfully obtain student's GWA")
+highest_grade = gwa.index(min(gwa))
+logging.info("successfully obtain student data who has highest GWA")
+print(f"{data_value[highest_grade].get('name')} got {data_value[highest_grade].get('gwa')} which is the highest in the class")
